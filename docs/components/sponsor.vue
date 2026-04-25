7626e3ad85fd58c46a7cdd84ea80edecb02dedcd
1024小神 <15670339118@163.com>
2026-04-25 16:22:34 +0800

    Update sponsorship descriptions in multiple languages

--- docs/components/sponsor.vue
+++ docs/components/sponsor.vue
@@ -123,7 +123,7 @@ const langMap: any = {
     zh: {
         title: '💖 赞助我们',
         description:
-            '如果您也喜欢我们的PakePlus项目，欢迎赞助我们，我们会继续开发和维护。您的支持是我们前进的动力。赞助时请务必填写留言或发邮箱：1024xiaoshen@gmail.com，以便我们收录到赞助名单中，感谢您的支持与鼓励！',
+            '如果您也喜欢我们的PakePlus项目，欢迎赞助我们，我们会继续开发和维护。您的支持是我们前进的动力。赞助时请务必填写留言或发邮箱：1024xiaoshen@gmail.com，以便我们收录到赞助名单中，并为您提供开通更多使用权限的激活码！',
         wechat: '微信/支付宝/爱发电赞助',
         wallet: 'Web3 虚拟币钱包赞助',
         copyAddress: '复制地址',
@@ -133,7 +133,7 @@ const langMap: any = {
     en: {
         title: '💖 Sponsor Us',
         description:
-            'If you also like our PakePlus project, we welcome your sponsorship to help us continue development and maintenance. Your support is our driving force. Please include a message or email us at: 1024xiaoshen@gmail.com when sponsoring so we can acknowledge your contribution. Thank you for your support!',
+            'If you also like our PakePlus project, we welcome your sponsorship to help us continue development and maintenance. Your support is our driving force. Please include a message or email us at: 1024xiaoshen@gmail.com when sponsoring so we can acknowledge your contribution and provide you with an activation code to unlock more features! Thank you for your support!',
         wechat: 'WeChat/Alipay Sponsorship',
         wallet: 'Web3 Wallet Support',
     },
